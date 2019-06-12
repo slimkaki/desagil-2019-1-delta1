0456378ef681b9c5eec3cebf5c7337912658bb49
slimkaki <rafaalmada98@gmail.com>
2019-06-12 15:50:13 -0300

    Questao 1 - Rafael Almada - Delta

--- src/main/java/br/pro/hashi/ensino/desagil/desafio/Controller.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/Controller.java
@@ -3,6 +3,7 @@ package br.pro.hashi.ensino.desagil.desafio;
 import br.pro.hashi.ensino.desagil.desafio.model.CpuPlayer;
 import br.pro.hashi.ensino.desagil.desafio.model.HumanPlayer;
 import br.pro.hashi.ensino.desagil.desafio.model.Model;
+import br.pro.hashi.ensino.desagil.desafio.model.Target;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -65,8 +66,10 @@ public class Controller implements KeyListener, ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         CpuPlayer cpuPlayer = model.getCpuPlayer();
+        Target target = model.getTarget();
 
         cpuPlayer.move();
+        target.randomMovement();
 
         view.repaint();
     }
--- src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
@@ -1,10 +1,121 @@
 package br.pro.hashi.ensino.desagil.desafio.model;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Target extends Element {
+
     private final Board board;
 
     public Target(int row, int col, Board board) {
         super(row, col);
         this.board = board;
+    }
+
+    private void move(int rowShift, int colShift) {
+        row += rowShift;
+        col += colShift;
+    }
+
+    private void moveUp() {
+        if (row>0 && !board.isWall(row - 1, col)) {
+            move(-1, 0);
+        }
+    }
+
+    private void moveRight() {
+        if (col < board.getNumCols() - 1 && !board.isWall(row, col + 1)) {
+            move(0, 1);
+        }
+    }
+
+    private void moveDown() {
+        if (row < board.getNumRows() - 1 && !board.isWall(row + 1, col)) {
+            move(1, 0);
+        }
+    }
+
+    private void moveLeft() {
+        if (col > 0 && !board.isWall(row,col - 1)) {
+            move(0, -1);
+        }
+    }
+
+    private LinkedList<String> possibleMovements() {
+        LinkedList<String> possibilitiesList = new LinkedList<>();
+
+        // Checando se tem Wall pra cima (UP -> (-1, 0))
+        if (row>0 && !board.isWall(row - 1, col)) {
+            possibilitiesList.add("UP");
+        }
+
+        // Checando se tem Wall pra direita (Right -> (0, 1))
+        if (col < board.getNumCols() - 1 && !board.isWall(row, col + 1)) {
+            possibilitiesList.add("RIGHT");
+        }
+
+        // Checando se tem Wall pra baixo (Down -> (1, 0))
+        if (row < board.getNumRows() - 1 && !board.isWall(row + 1, col)) {
+            possibilitiesList.add("DOWN");
+        }
+
+        // Checando se tem Wall pra esquerda (Left -> (0, -1))
+        if (col > 0 && !board.isWall(row,col - 1)) {
+            possibilitiesList.add("LEFT");
+        }
+
+        return possibilitiesList;
+    }
+
+    public void randomMovement() {
+        LinkedList<String> possible = possibleMovements();
+        if (possible.size() == 1) {
+            String string = possible.element();
+            switch (string) {
+                case "UP":
+                    moveUp();
+                    break;
+
+                case "RIGHT":
+                    moveRight();
+                    break;
+
+                case "DOWN":
+                    moveDown();
+                    break;
+
+                case "LEFT":
+                    moveLeft();
+                    break;
+            }
+
+        } else {
+            Random random = new Random();
+            int nextStep = random.nextInt(possible.size());
+            int counter = 0;
+            for (String string : possible) {
+                if (counter == nextStep) {
+                    switch (string) {
+                        case "UP":
+                            moveUp();
+                            break;
+
+                        case "RIGHT":
+                            moveRight();
+                            break;
+
+                        case "DOWN":
+                            moveDown();
+                            break;
+
+                        case "LEFT":
+                            moveLeft();
+                            break;
+                    }
+                    break;
+                }
+                counter++;
+            }
+        }
     }
 }
